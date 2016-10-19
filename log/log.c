f404878ce11bf8c9294ad60b5e6b364547a7a082
Vlad Vovchenko <vlad.vovchenko93@gmail.com>
2016-10-20 02:57:04 +0300

    socket reading

--- log/log.c
+++ log/log.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
+#include <unistd.h>
 #include <errno.h>
-#include <string.h>
-#include <getopt.h>
 #include <pthread.h>
+#include <sys/un.h>
+#include <sys/socket.h>
 #include <systemd/sd-bus.h>
 
 #define DBUS_MAX_NAME_LEN 256
@@ -13,11 +13,14 @@ const char *objectmapper_service_name =  "org.openbmc.ObjectMapper";
 const char *objectmapper_object_name  =  "/org/openbmc/ObjectMapper";
 const char *objectmapper_intf_name    =  "org.openbmc.ObjectMapper";
 
-static char *msg;
+static char *buffer;
+static size_t buffer_sz;
+static size_t buffer_capacity;
+static const size_t buffer_init_capacity = 16 * 1024;
 
 static char *obmc_console_read(void)
 {
-	return msg;
+	return buffer;
 }
 
 static int obmc_console_function_router(sd_bus_message *msg, void *user_data,
@@ -28,7 +31,7 @@ static int obmc_console_function_router(sd_bus_message *msg, void *user_data,
 	/* get the operation. */
 	const char *obmc_console_function = sd_bus_message_get_member(msg);
 	if (!obmc_console_function) {
-		fprintf(stderr, "log: Null uart function specificed\n");
+		fprintf(stderr, "NULL obmc_console function specificed\n");
 		return sd_bus_reply_method_return(msg, "i", -1);
 	}
 
@@ -51,8 +54,52 @@ static const sd_bus_vtable obmc_console_vtable[] =
 
 static void *socket_th_function(void *args)
 {
-	fprintf(stderr, "log: Thread created!");
+	static const char console_socket_path[] = "\0obmc-console";
+	static const size_t console_socket_path_len = 
+		sizeof(console_socket_path) - 1;
+	struct sockaddr_un addr;
+	int fd;
+	char c;
+       
+	fd = socket(AF_UNIX, SOCK_STREAM, 0);
+	if (fd == -1) {
+		perror("Failed to create socket");
+		goto out;
+	}
+	memset(&addr, 0, sizeof(addr));
+	addr.sun_family = AF_UNIX;
+	memcpy(&addr.sun_path, &console_socket_path, console_socket_path_len);
 
+	if (connect(fd, (struct sockaddr *)&addr, sizeof(addr))) {
+		perror("Failed to connect to console server");
+		goto close_sock;
+	}
+
+	buffer = malloc(buffer_init_capacity);
+	if (!buffer) {
+		fprintf(stderr, "Failed to allocate memory\n");
+		goto close_sock;
+	}
+	buffer[0] = '\0';
+	buffer_sz = 0;
+	buffer_capacity = buffer_init_capacity;
+
+	for (;;) {
+		if (read(fd, &c, 1) < 1)
+			break;
+		if (buffer_sz + 1 == buffer_capacity) {
+			/* if there is not enought space for new data */
+			fprintf(stderr, "Buffer is full\n");
+			goto close_sock;
+		}
+		buffer[buffer_sz] = c;
+		buffer_sz++;
+	}
+
+	fprintf(stderr, "exit socket thread\n");
+ close_sock:
+	close(fd);
+ out:
 	return NULL;
 }
 
@@ -67,7 +114,8 @@ int main(int argc, char **argv)
 
 	rc = sd_bus_open_system(&bus);
 	if (rc < 0) {
-		fprintf(stderr,"log: Error opening system bus.\n");
+		fprintf(stderr,"Error opening system bus: %s\n",
+			strerror(-rc));
 		return rc;
 	}
 
@@ -75,20 +123,22 @@ int main(int argc, char **argv)
 			obmc_console_object, obmc_console_iface,
 			obmc_console_vtable, NULL);
 	if (rc < 0) {
-		fprintf(stderr, "log: Failed to add object to dbus: %s\n",
-				strerror(-rc));
+		fprintf(stderr, "Failed to add object to dbus: %s\n",
+			strerror(-rc));
 		return rc;
 	}
 
 	rc = sd_bus_request_name(bus, obmc_console_iface, 0);
 	if (rc < 0) {
-		fprintf(stderr, "log: Failed to acquire service name: %s\n",
-				strerror(-rc));
+		fprintf(stderr, "Failed to acquire service name: %s\n",
+			strerror(-rc));
 		return rc;
 	}
 
-	if (pthread_create(&socket_thread, NULL, socket_th_function, NULL)) {
-		fprintf(stderr, "log: Failed to create thread\n");
+	rc = pthread_create(&socket_thread, NULL, socket_th_function, NULL);
+	if (rc < 0) {
+		fprintf(stderr, "Failed to create thread: %s\n",
+			strerror(rc));
 		rc = 1;
 		goto sdbus_free;
 	}
@@ -120,3 +170,4 @@ int main(int argc, char **argv)
 
 	return rc;
 }
+
